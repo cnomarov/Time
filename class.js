8254723e9c7c0d9084c01fca4aceef7609de5a8d
cnomarov <cnomarov@gmail.com>
2025-01-10 14:03:45 +0500

    Update class.js

--- class.js
+++ class.js
@@ -45,7 +45,7 @@ class Clock {
 
     start() {
         this.render();
-        this.timer = setInterval(() => this.render(), 1000);
+        this.timer = setInterval(() => this.render(), 100);
     };
 }
 
